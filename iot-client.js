d5ebaf83bc356d11836551d2dd0c9f92d61c2661
Akhthaar Ali Badhusha <badhusha.s@outlook.com>
2025-05-08 07:01:26 +0000

    Working example: After this is a test

---
+++ iot-client.js
@@ -0,0 +1,4 @@
+// const awsIoTClient = new iotClientSDK.IoTClient({
+	// 	region: 'us-east-1',
+	// 	credentials: { accessKeyId: 'AKIAWESZ4I5YDD5IAWF7', secretAccessKey: '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' },
+	// })
--- srv/admin-service.js
+++ srv/admin-service.js
@@ -1,75 +1,88 @@
 const cds = require("@sap/cds");
 const iotClientSDK = require('@aws-sdk/client-iot')
 
-module.exports = cds.service.impl(async (srv) => {
+module.exports = async (srv) => {
 	const db = await cds.connect.to("db");
 	const { Greenhouse, Sensors } = srv.entities;
+	console.log('Custom handler loaded');
 
-	const awsIoTClient = new iotClientSDK.IoTClient({
-		region: 'us-east-1',
-		credentials: { accessKeyId: 'AKIAWESZ4I5YDD5IAWF7', secretAccessKey: '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' },
-	})
+	
+	// async function createThing(sensorId) {
+	// 	return new Promise(async (resolve, reject) => {
 
-	async function createThing(sensorId) {
-		return new Promise(async (resolve, reject) => {
+	// 		try {
+	// 			let input = { thingName: sensorId }
+	// 			let command = new iotClientSDK.CreateThingCommand(input)
+	// 			let response = await awsIoTClient.send(command)
+	// 			resolve(response)
+	// 		} catch (error) {
+	// 			reject(error)
+	// 		}
 
-			try {
-				let input = { thingName: sensorId }
-				let command = new iotClientSDK.CreateThingCommand(input)
-				let response = await awsIoTClient.send(command)
-				resolve(response)
-			} catch (error) {
-				reject(error)
-			}
+	// 	});
+	// }
+	// function createCertificate() {
+	// 	return new Promise(async (resolve, reject) => {
 
-		});
-	}
-	function createCertificate() {
-		return new Promise(async (resolve, reject) => {
+	// 		try {
+	// 			let input = { setAsActive: true }
+	// 			let command = new iotClientSDK.CreateKeysAndCertificateCommand(input)
+	// 			let response = await awsIoTClient.send(command)
+	// 			resolve(response)
+	// 		} catch (error) {
+	// 			reject(error)
+	// 		}
 
-			try {
-				let input = { setAsActive: true }
-				let command = new iotClientSDK.CreateKeysAndCertificateCommand(input)
-				let response = await awsIoTClient.send(command)
-				resolve(response)
-			} catch (error) {
-				reject(error)
-			}
+	// 	});
+	// }
+	// function attachCertificate2Thing(sensorId, certificateARN) {
+	// 	return new Promise(async (resolve, reject) => {
 
-		});
-	}
-	function attachCertificate2Thing(sensorId, certificateARN) {
-		return new Promise(async (resolve, reject) => {
+	// 		try {
+	// 			let input = { thingName: sensorId,principal: certificateARN }
+	// 			let command = new iotClientSDK.AttachThingPrincipalCommand(input)
+	// 			let response = await awsIoTClient.send(command)
+	// 			resolve(response)
+	// 		} catch (error) {
+	// 			reject(error)
+	// 		}
 
-			try {
-				let input = { thingName: sensorId,principal: certificateARN }
-				let command = new iotClientSDK.AttachThingPrincipalCommand(input)
-				let response = await awsIoTClient.send(command)
-				resolve(response)
-			} catch (error) {
-				reject(error)
-			}
+	// 	});
+	// }
+	// function attachCertificate2Policy(policyName, certificateARN) {
+	// 	return new Promise(async (resolve, reject) => {
 
-		});
-	}
-	function attachCertificate2Policy(policyName, certificateARN) {
-		return new Promise(async (resolve, reject) => {
+	// 		try {
+	// 			let input = { policyName,target: certificateARN }
+	// 			let command = new iotClientSDK.AttachPolicyCommand(input)
+	// 			let response = await awsIoTClient.send(command)
+	// 			resolve(response)
+	// 		} catch (error) {
+	// 			reject(error)
+	// 		}
 
-			try {
-				let input = { policyName,target: certificateARN }
-				let command = new iotClientSDK.AttachPolicyCommand(input)
-				let response = await awsIoTClient.send(command)
-				resolve(response)
-			} catch (error) {
-				reject(error)
-			}
-
-		});
-	}
+	// 	});
+	// }
 
 
-	srv.after('CREATE', Sensors.drafts, async (data,req) => {
-		console.log(data)
-		console.log(req)
-	})
-});
+	// srv.after('UPDATE', ['Sensors.drafts','Sensors'], async (data,req) => {
+	// 	console.log("$$$$$$$$$$$$$$$$")
+	// 	console.log(data)
+	// 	console.log(req)
+	// })
+}
+
+// 	async init() {
+// 		const db = await cds.connect.to("db");
+// 		const { Greenhouse, Sensors } = this.entities;
+// 		console.log("Handler Loaded")
+
+// 		this.after([CREATE,UPDATE],'*',async (req)=>{
+// 			console.log('******************************************************************************')
+// 			console.log(JSON.stringify(req.data))
+// 		})
+// 		await super.init();
+// 	}
+// }
+
+// module.exports = GreenhouseService
